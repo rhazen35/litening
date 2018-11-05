11ecb7c1a69296a6ebf7b9a9edd915c779d87043
rhazen35 <rhazen35@gmail.com>
2018-11-05 22:03:42 +0100

    Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,11 +6,48 @@
 
         <title>{{ config('app.name') }}</title>
 
+        <!-- Stylesheets -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css">
+
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
     </head>
     <body>
-        <div class="">{{ config('app.name') }}</div>
+        <header>
+            <div class="page-title">{{ config('app.name') }} Web Solutions</div>
+            <div class="page-subtitle">There are no problems, only opportunities.</div>
+        </header>
+
+        <main>
+            <div class="main-title">We build effective web solutions to support your growth.</div>
+            <div class="main-infotext">
+                <p>
+                    SEO, UX/UI, Responsive and Fast. You can expect cooperative collaboration.
+                    We encourage you to be critical about what we build for you.
+                    <br>
+                    From an informative, purposeful website to a complex Webshop or Web Suite. 
+                    <br>
+                    All possibilities are open.
+                </p>
+            </div>
+
+            <div class="main-portrait-img">
+                <img src="{{ asset('img/ruben-hazenbosch.jpg') }}">
+            </div>
+
+            <div class="main-title">We use the powerful Laravel Framework and the latest technologies.</div>
+            <div class="main-infotext">
+                <p>
+                    PHP 7.2, Javascript: ECMAScript 6 Modules and Ajax. CSS3, SASS, and much, much more!
+                    <br>
+
+                </p>
+            </div>
+
+            <div class="main-img">
+                <img src="{{ asset('img/laravel.png') }}">
+            </div>
+        </main>
     </body>
 </html>
